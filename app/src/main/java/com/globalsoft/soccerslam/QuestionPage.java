e0ed45a1e8ca5526ea80347c4992d4cbf5bc197e
brownwood <brownwoodinc@hotmail.com>
2016-04-07 10:45:27 +0100

    Commit on 07/04/2016

---
+++ app/src/main/java/com/globalsoft/soccerslam/QuestionPage.java
@@ -0,0 +1,15 @@
+package com.globalsoft.soccerslam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Adebowale.Odulaja on 4/6/2016.
+ */
+public class QuestionPage extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+    }
+}
